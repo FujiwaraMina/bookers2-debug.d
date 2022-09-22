dd70bf7736ff6ee44f4a467d936fe53307b997cc
EC2 Default User <ec2-user@ip-172-31-39-55.ap-northeast-1.compute.internal>
2022-09-22 09:16:42 +0000

    [Fnish]

--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -1,4 +1,10 @@
 class BookComment < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  has_one_attached :profile_image
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
 end
