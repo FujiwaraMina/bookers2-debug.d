fbbaebf87191529bc2cea741f3d46cb1fd864bdf
EC2 Default User <ec2-user@ip-172-31-39-55.ap-northeast-1.compute.internal>
2022-10-09 15:14:02 +0000

    [Add9d]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,17 +5,21 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @book_comment = BookComment.new
+    @book_tags = @book.tags
   end
 
   def index
-    @books = Book.all
+    @books = Book.page(params[:page]).per(10)
     @book = Book.new
+    @tag_list = Tag.all
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    tag_list = params[:book][:name].split(',')
     if @book.save
+       @book.save_tag(tag_list)
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
@@ -25,6 +29,7 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    @tag_list = @book.tags.plunk(:name).join(',')
     if @book.user == current_user
       render "edit"
     else
@@ -34,7 +39,9 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
+    @tag_list = params[:book][:name].split(',')
     if @book.update(book_params)
+       @book.save_tag(tag_list)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
       render "edit"
@@ -45,6 +52,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
+  end
+
+  def search_tag
+    @tag_list = Tag.all
+    @tag = Tag.find(params[:tag_id])
+    @books = @tag/posts.page(params[:page]).per(10)
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -50,4 +50,11 @@ has_many :following_user, through: :follower, source: :followed
       @user = User.all
     end
   end
+
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
 end
